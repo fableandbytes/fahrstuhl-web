b49eb2c7dbec8a11c832f8bd27898a56a08c4b7f
Fable And Bytes <fable.and.bytes@gmail.com>
2026-04-28 04:51:03 +0200

    Fix: Resolve Binaryen dependency issue by allowing project repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,40 +15,18 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    // PREFER_PROJECT ist wichtig, damit das Kotlin-Plugin für WasmJS interne Repos (wie für Binaryen) hinzufügen kann
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
         maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev") }
         maven { url = uri("https://androidx.dev/storage/compose-compiler/repository") }
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
-        ivy("https://nodejs.org/dist") {
-            name = "Node.js Distributions"
-            patternLayout {
-                artifact("v[revision]/[artifact]-v[revision]-[classifier].[ext]")
-            }
-            metadataSources {
-                artifact()
-            }
-            content {
-                includeGroup("org.nodejs")
-            }
-        }
-        ivy("https://github.com/yarnpkg/yarn/releases/download") {
-            name = "Yarn Distributions"
-            patternLayout {
-                artifact("v[revision]/[artifact]-v[revision].[ext]")
-            }
-            metadataSources {
-                artifact()
-            }
-            content {
-                includeGroup("com.yarnpkg")
-            }
-        }
+        // Dieses Repo enthält oft die benötigten Wasm-Tools
+        maven { url = uri("https://packages.jetbrains.team/maven/p/firework/dev") }
     }
 }
 
 rootProject.name = "FahrstuhlKartenspiel"
 include(":app")
- 
